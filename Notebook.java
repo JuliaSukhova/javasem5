6a6c11826223776757841a126fd8b9cdf1a18878
Julia Sukhova <suxova1998@gmail.com>
2023-08-01 17:08:55 +0300

    Класс

---
+++ Notebook.java
@@ -0,0 +1,17 @@
+public class Notebook {
+    private String brand;
+    private int ramSizeGB;
+    private int storageSizeGB;
+    private String os;
+    private String color;
+
+    public Notebook(String brand, int ramSizeGB, int storageSizeGB, String os, String color) {
+        this.brand = brand;
+        this.ramSizeGB = ramSizeGB;
+        this.storageSizeGB = storageSizeGB;
+        this.os = os;
+        this.color = color;
+    }
+
+    // Геттеры и сеттеры (если необходимо)
+}
